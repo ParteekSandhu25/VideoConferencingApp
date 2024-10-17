c9fbabf2d9f584fcd659045cdd47db8bbd979cd0
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2024-10-17 08:07:01 +0530

    meeting calls setup, created custom hook

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
 const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div className="text-white text-2xl">The meeting id: {params.id}</div>;
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <div className="text-white text-2xl">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </div>
+  );
 };
 
 export default Meeting;
