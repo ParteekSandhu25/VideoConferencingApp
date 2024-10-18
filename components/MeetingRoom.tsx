748ae3202a44ea252ee2d2a9fcd9034539792d8a
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2024-10-18 16:16:15 +0530

    added copy meeting url button in meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@/lib/utils";
 import {
   CallControls,
@@ -17,9 +18,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { LayoutList, Users } from "lucide-react";
+import { Copy, LayoutList, Users } from "lucide-react";
 import EndCallButton from "./EndCallButton";
 import Loader from "./Loader";
+import { toast } from "@/hooks/use-toast";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
@@ -60,7 +62,7 @@ const MeetingRoom = () => {
         </div>
       </div>
 
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+      <div className="fixed bottom-0 flex-wrap flex w-full items-center justify-center gap-5">
         <CallControls onLeave={() => router.push(`/`)} />
         <DropdownMenu>
           <div className="flex items-center">
@@ -84,6 +86,18 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            toast({
+              title: "Link Copied",
+            });
+          }}
+        >
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+            <Copy size={20} className="text-white" />
+          </div>
+        </button>
         <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
